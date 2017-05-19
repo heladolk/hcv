596f082fa928441ff75fce87c1059dc5bb71ad25
Winnie Kwon <lkwon@pivotal.io>
2017-05-18 17:41:14 -0700

    restructure the app and add region-summary component

---
+++ frontend/src/components/usmap.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class USMap extends React.component {
+  render() {
+    return (
+      <div>USMap</div>
+    )
+  }
+}
--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import RegionSummary from './components/region-summary'
+// import USMap from './components/usmap'
 
 export class Home extends React.Component {
   render () {
     return (
-      <div className="home">
-        React + Webpack + Babel + Jest + Spring Starter kit
+      <div className="row home">
+        <div className="row">
+          <div id="summary-box">
+            <div className="item-box">
+              <RegionSummary/>
+            </div>
+          </div>
+          <div className="item-box" id="map-box">
+            {/*<USMap />*/}
+          </div>
+        </div>
       </div>
     );
   }
